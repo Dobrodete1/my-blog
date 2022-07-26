79b0e44d8d9363d9bbac881be5fb11bbae5ccd14
Dobrodete1 <kozho31203@gmail.com>
2022-07-26 09:13:29 +0600

    feat: 26-07

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import React from 'react'
-import Footer from './components/Footer';
-import Header from './components/Header';
-import BlogEndries from './components/BlogEndries';
-import IntrodactionMenu from './components/IntroductionMenu/IntrodactionMenu';
+import React from "react";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import BlogEndries from "./components/BlogEndries";
+import IntrodactionMenu from "./components/IntroductionMenu/IntrodactionMenu";
 
 const App = () => {
   return (
     <>
-    <div className="w3-content" style={{maxWidth: 1400}}>
-      <Header/>
-      <div className="w3-row">
-        <BlogEndries/>
-        <IntrodactionMenu/>
+      <div className="w3-content" style={{ maxWidth: 1400 }}>
+        <Header />
+        <div className="w3-row">
+          <BlogEndries />
+          <IntrodactionMenu />
+        </div>
       </div>
-    </div>
-    <Footer/>
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
